13a2bbec5cf9dc19eeb75c2315bbb9c3354aec14
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-16 21:54:25 -0400

    feat: check_bit.py

--- bitwise-operations/common-use-cases/py/check_bit.py
+++ bitwise-operations/common-use-cases/py/check_bit.py
@@ -8,3 +8,8 @@
 # Example:
 #   n = 5 (0101), pos = 2 -> (5 >> 2) & 1 = 1 & 1 = 1  -> bit is set
 #   n = 5 (0101), pos = 1 -> (5 >> 1) & 1 = 2 & 1 = 0  -> bit is not set
+
+if __name__ == "__main__":
+    num = int(input())
+    pos = int(input())
+    print((num >> (pos)) & 1)
