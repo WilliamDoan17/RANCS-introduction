34dc2cd42ed9e97ebfb0fdc8113e69beb8ed4f7d
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-17 15:48:00 -0400

    feat: hamming_weight.py

--- bitwise-operations/practice/py/hamming_weight.py
+++ bitwise-operations/practice/py/hamming_weight.py
@@ -4,3 +4,14 @@
 #
 # Example:
 #   n = 11 -> 3
+
+if __name__ == "__main__":
+    n = int(input())
+
+    count = 0
+
+    while (n != 0):
+        count += (n & 1)
+        n >>= 1
+
+    print(count)
