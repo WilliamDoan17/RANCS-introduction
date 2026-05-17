76fcd404408889df6626eeff2cbce2cfb7fdd57d
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-16 22:21:04 -0400

    feat: even_odd_check.cpp

--- bitwise-operations/common-use-cases/cpp/even_odd_check.cpp
+++ bitwise-operations/common-use-cases/cpp/even_odd_check.cpp
@@ -4,3 +4,13 @@
 // Example:
 //   n = 5 -> 1 (odd)
 //   n = 4 -> 0 (even)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cin >> num;
+
+  cout << (num & 1) << "\n";
+}
