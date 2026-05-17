ef0a383aa542cbee16e05df4523c5844108857b6
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-17 15:45:20 -0400

    feat: counting_bits.cpp

--- bitwise-operations/practice/cpp/counting_bits.cpp
+++ bitwise-operations/practice/cpp/counting_bits.cpp
@@ -4,3 +4,24 @@
 //
 // Example:
 //   n = 5 -> [0, 1, 1, 2, 1, 2]
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int result[n + 1];
+
+  result[0] = 0;
+
+  for (int i = 1; i <= n; i++) {
+    result[i] = (i & 1) + result[i >> 1];
+  }
+
+  for (int i = 0; i <= n; i++) {
+    cout << result[i] << " ";
+  }
+  cout << "\n";
+}
