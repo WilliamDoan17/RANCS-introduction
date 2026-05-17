1bdcc5029257753523c4fbe0e5cdeeb85b3bb5a2
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-17 16:01:11 -0400

    feat: missing_number.cpp

--- bitwise-operations/practice/cpp/missing_number.cpp
+++ bitwise-operations/practice/cpp/missing_number.cpp
@@ -4,3 +4,23 @@
 //
 // Example:
 //   [3, 0, 1] -> 2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int result = 0;
+
+  for (int i = 0; i < n; i++) {
+    int a;
+    cin >> a;
+    result ^= (a ^ i);
+  }
+
+  result ^= n;
+
+  cout << result << "\n";
+}
