9bce9fc944d3c561ccb946f4251a643541011edf
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-20 23:27:21 -0400

    feat: power_of_two.cpp

--- bitwise-operations/practice/cpp/power_of_two.cpp
+++ bitwise-operations/practice/cpp/power_of_two.cpp
@@ -1,8 +1,19 @@
 // Problem: Power of Two (LeetCode 231)
-// Given an integer n, return true if it is a power of two, otherwise return false.
+// Given an integer n, return true if it is a power of two, otherwise return
+// false.
 //
 // Example:
 //   n = 8 -> true
 //   n = 6 -> false
 //   n = 1 -> true
 //   n = 0 -> false
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << (n > 0 && (n & (n - 1)) == 0);
+}
