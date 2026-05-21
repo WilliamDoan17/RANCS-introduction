2b636aade1e42d162253efdc6dac3d556475bb8d
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-20 23:30:29 -0400

    feat: add_without_plus.cpp & hamming_weight.cpp
    Co-Authored-By: WilliamDoan17 <phongjesus@gmail.com>

--- bitwise-operations/practice/cpp/hamming_weight.cpp
+++ bitwise-operations/practice/cpp/hamming_weight.cpp
@@ -4,3 +4,19 @@
 //
 // Example:
 //   n = 11 -> 3
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int n;
+  cin >> n;
+
+  int count = 0;
+  while (n != 0) {
+    count += (n & 1);
+    n >>= 1;
+  }
+
+  cout << count;
+}
