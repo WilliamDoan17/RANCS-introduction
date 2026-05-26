f680372ac303ed46cd3674278b5ae61dfe9179c8
WilliamDoan17 <pdoanthe96@gmail.com>
2026-05-26 16:49:04 -0400

    feat: with-library little-endian cpp

---
+++ pack-and-unpack-packets/with-library/little-endian/py/client.py
@@ -0,0 +1,30 @@
+import socket
+import struct 
+from enum import Enum
+
+class DataType(Enum):
+    INT = ord('i')
+    FLOAT = ord('f')
+    STRING = ord('s')
+
+def pack_data(data: bytes, data_type: DataType) -> bytes:
+    return bytes([data_type.value]) + data
+
+def from_int_to_le(data: int) -> bytes:
+    return struct.pack('<i', data)
+
+def from_float_to_le(data: float) -> bytes:
+    return struct.pack('<f', data)
+
+if __name__ == "__main__":
+    server_hostname = input("Enter sever hostname: ")
+    server_port = int(input("Enter server port: "))
+
+    server_addr = (server_hostname, server_port)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        data_type = DataType(ord(input("Specify data type to send data ('i' for int, 'f' for float, 's' for string): ")))
+
+
+
+
